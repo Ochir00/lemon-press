5ad1a04e4f9cf9dd53e754d452363b7627604984
24LP2291 24LP2291 <24LP2291@G304CC0215.local>
2024-12-19 11:03:58 +0800

    new start

--- src/NewApp.jsx
+++ src/NewApp.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "./style.css";
 import Header from "./componets/header";
-import News from "./componets/news";
-import HomeNews from "./componets/HomeNews";
-import BodyNews from "./componets/BodyNews";
+import Webheadernews from "./componets/Webheadernews";
+
+
+
 
 const App = () => {
   return (
     <div className="WebBody">
       <Header />
-      <div className="Body">
-        <News />
-        <HomeNews />
-        <BodyNews />
-      </div>
+      <Webheadernews />
     </div>
   );
 };
+
+
 
 export default App;
--- src/componets/news.jsx
+++
@@ -1,8 +0,0 @@
-const News = () => {
-    return (
-        <div className="Web-title-news">
-            <img src="https://cdn.boost.mn/66d672fbb1eb1b3f1bdc87e1/creative/efc9e90c-0421-405e-af8c-9404fe68cb68.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=prod-boost-sa%40aqueous-cabinet-374702.iam.gserviceaccount.com%2F20241217%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20241217T232520Z&X-Goog-Expires=86399&X-Goog-Signature=67dc8a0a46e3c235fdb0df6e15f24ea2632cfbbd396aee6aed4c273610c024c385a47ea1c613f2cadd4c794fda78a2bf7023b85e204f3615e30cc67880a2b36d2e9b00ae30f70fe32cbbdd210877baa8b5948524fd409bd223f4d063dc238a04c539a55bd343d247263733efbd2f9a112e992b428faa221793edbb34f11086e7f766a70975763d80eaa2c4a2f12a5faf6f7277eeb291eac30c029bcd105cea27a9b53e392406ae8559c066957ab1c2497af6537a4e69992b09af60cbe6f8f52b029aa63c7709a2b2326d9208e9e5862f9557857d9dc741c00dd5a6119095adbc63539389578072769120ea8087fbdef61f60af72d97302b224c78565e471c800&X-Goog-SignedHeaders=host" alt="..loading" />
-        </div>
-    )
-}
-export default News;
