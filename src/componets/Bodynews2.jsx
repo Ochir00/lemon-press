78be06c171fc00c5f589b8f90393f2221ae0c9d1
24LP2291 24LP2291 <24LP2291@G304CC0215.local>
2024-12-20 08:50:35 +0800

    cart 1 dezine

--- src/componets/Bodynews2.jsx
+++ src/componets/Bodynews2.jsx
@@ -3,8 +3,109 @@ const bodynews1 = () => {
         <div className="container">
             <div className="bodynews1">
                 <div className="cart1">
-                    <div className="cart1-head">
-                        
+                    <div className="cart1-border">
+                        <div className="cart1-border-head">
+                            <p>Товхимлууд</p>
+                        </div>
+                        <div className="cart1-border-body">
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>😷🥲 Шийдэл: Яндангүй бүс</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>✈️🌍 Нэг тэнгэрт нэг л компани</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🇰🇿🇲🇳 Ижил замаар ялгаатай хурдаар</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🪙 Крипто самурай Нью-Йоркт</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🤜 Дэлхийд буурч, Монголд өснө тэр юу вэ?</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🔮🪄 Accio: Тэр эргэн ирнэ</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🇲🇳 Apple Pay Монголд</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🤡 Монголын ТОП асуудал: Өөртөө хэт итгэлтэй</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 Худалдааны дайн 2.0</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>🤥 Сандал ширээний онц байдал</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+                            <div className="cart1-frame1">
+                                <p>Wed, 18 Dec 2024 23:00:00 +0000</p>
+                                <p>📢 “Ухаалаг засаг” II = $40 тэрбум</p>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
                 <div className="cart1"></div>
